1d2f1d0477e55e344224e4e009ec37932e5ab51e
Supanut Apikulvanich <supanut.overload@gmail.com>
2016-12-14 16:39:47 +0700

    :heavy_plus_sign: Added redux-logger

--- src/redux/create.js
+++ src/redux/create.js
@@ -1,34 +1,33 @@
-import { createStore as _createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createMiddleware from './middleware/clientMiddleware';
 import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
 import Immutable from 'immutable';
+import createLogger from 'redux-logger';
 
-export default function createStore(history, client, data) {
+export default function configureStore(history, client, data) {
   // Sync dispatched route actions to the history
   const reduxRouterMiddleware = routerMiddleware(history);
 
-  const middleware = [createMiddleware(client), reduxRouterMiddleware, thunk];
+  const middlewares = [createMiddleware(client), reduxRouterMiddleware, thunk];
+  const tools = [];
 
-  let finalCreateStore;
   if (__DEVELOPMENT__ && __CLIENT__ && __DEVTOOLS__) {
+    middlewares.push(createLogger());
     const { persistState } = require('redux-devtools');
     const DevTools = require('../containers/DevTools/DevTools');
-    finalCreateStore = compose(
-      applyMiddleware(...middleware),
-      window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument(),
-      persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
-    )(_createStore);
-  } else {
-    finalCreateStore = applyMiddleware(...middleware)(_createStore);
+    tools.push(window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument());
+    tools.push(persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)));
   }
 
   const reducer = require('./modules/reducer');
   if (data) {
     data.pagination = Immutable.fromJS(data.pagination);
   }
-  const store = finalCreateStore(reducer, data);
-
+  const store = createStore(reducer, data, compose(
+    applyMiddleware(...middlewares),
+    ...tools,
+  ));
 
   if (__DEVELOPMENT__ && module.hot) {
     module.hot.accept('./modules/reducer', () => {
